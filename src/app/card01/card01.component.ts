8a418c143e1307f450ae0540075149e3ee9b8acb
Arthur Gabriel <arthurgabrielmv@gmail.com>
2024-03-03 22:33:44 -0300

    initial commit

---
+++ src/app/card01/card01.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card01',
+  standalone: true,
+  imports: [],
+  templateUrl: './card01.component.html',
+  styleUrl: './card01.component.css'
+})
+export class Card01Component {
+
+}
